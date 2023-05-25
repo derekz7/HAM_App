e7da789c681112ef73dc1bc5c12fb7baefc75f7e
Đức Lee <duclee.dz@gmail.com>
2023-05-26 01:08:04 +0700

    main menu activity added

---
+++ app/src/main/java/com/example/ham_app/activities/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ham_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.example.ham_app.R;
+import com.example.ham_app.databinding.ActivityMainBinding;
+import com.example.ham_app.fragments.AccountFragment;
+import com.example.ham_app.fragments.AppointmentFragment;
+import com.example.ham_app.fragments.HomeFragment;
+import com.example.ham_app.fragments.ProfileFragment;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            final int itemId = item.getItemId();
+            if (itemId == R.id.trangchu) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.appointment) {
+                replaceFragment(new AppointmentFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (itemId == R.id.account) {
+                replaceFragment(new AccountFragment());
+            }
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+}
